16a95a72add710ab2a32a3df879e555a6cb08694
desktop <psomail@yandex.ru>
2023-01-28 16:48:41 +0300

    start2

--- task9_1/main.py
+++ task9_1/main.py
@@ -1,20 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import telebot
+from telebot import types
 
-bot = telebot.TeleBot
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+bot = telebot.TeleBot("5954559514:AAFArRpHdm6n4T4QcEtFzkvBltjrxGkoSW4")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, message)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+bot.infinity_polling()
