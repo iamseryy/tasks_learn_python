83278ad81d9c37ec59cb791124b052a04be6a388
desktop <psomail@yandex.ru>
2023-01-28 18:31:44 +0300

    start2

--- task9_1/main.py
+++ task9_1/main.py
@@ -6,7 +6,15 @@ bot = telebot.TeleBot("5954559514:AAFArRpHdm6n4T4QcEtFzkvBltjrxGkoSW4")
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, message)
+    bot.send_message(message.chat.id, 'Напиши имя и фамилию')
+    bot.register_next_step_handler(message, sentence)
+
+
+def sentence(message):
+    text = message.text
+    surname = text.split()[0]
+    name = text.split()[1]
+    bot.send_message(message.chat.id, f"Вас зовут: {name}, фамилия: {surname}")
 
 
 bot.infinity_polling()
